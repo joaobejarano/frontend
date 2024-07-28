bf703cecbb2169b393b8be9a334255b9492251d7
joaobejarano <joaom.bejarano@gmail.com>
2024-07-28 15:46:54 -0300

    Create the  List and Item Component

---
+++ src/ListComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ItemComponent from './ItemComponent';
+
+
+export default function ListComponent(){
+    return (
+        <div>
+            <h2>Minha Lista</h2>
+            <ul>
+                <ItemComponent name={'Meu Item'} />
+                <ItemComponent name={'Meu Item 2'} />
+            </ul>
+        </div>
+        
+    )
+}
